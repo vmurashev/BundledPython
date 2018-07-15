f8136182a36f1445109637ab0cae54f9214fce51
vmurashev <vitaly.murashev@gmail.com>
2018-07-15 17:45:49 +0300

    move stdlib_testsuite.inc

--- cpython2/publish/testsuite/minibuild.mk
+++ cpython2/publish/testsuite/minibuild.mk
@@ -10,7 +10,7 @@ else:
    PYSTDLIB_SUBDIR = 'python{}'.format(BUNDLED_PYTHON_VERSION_DOT)
 
 composite_spec += [
-  ['${@project_root}/cpython2/build/stdlib/stdlib_testsuite.inc', {'file': True, 'spec_file': True, 'subdir': PYSTDLIB_SUBDIR}],
+  ['stdlib_testsuite.inc', {'file': True, 'spec_file': True, 'subdir': PYSTDLIB_SUBDIR}],
   ['${@project_root}/cpython2/build/modules/testcapi', PYMOD_PROPERTIES],
   ['${@project_root}/cpython2/build/modules/ctypes_test', PYMOD_PROPERTIES],
 ]
